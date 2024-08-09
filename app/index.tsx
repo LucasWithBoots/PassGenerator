dab838bb036a9fc1806bd791dc26626edc94b7e0
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-08-08 23:57:25 -0300

    feat: estrutura básica e estilos no tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import {
+  StatusBar,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { passwordStrength } from "check-password-strength";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={"#fed7aa"} />
+      <View className="flex-1 items-center justify-center bg-orange-200">
+        <View className="bg-green-200 w-80 h-80 border-4 rounded-lg z-20 p-4">
+          <View className="bg-stone-200 h-14 rounded-lg justify-center">
+            <Text>asdhasd@$$@</Text>
+          </View>
+
+          <View className="h-2 bg-stone-200 mt-2 z-10">
+            <View className="h-2 w-1/2 bg-red-500 z-20"></View>
+          </View>
+
+          <View>
+            <Text>Password Lenght</Text>
+            <Text>Contains Uppercase</Text>
+            <Text>Contains Lowercase</Text>
+            <Text>Contains Numbers</Text>
+            <Text>Contains Symbols</Text>
+          </View>
+
+          <TouchableOpacity className="bg-black h-10 rounded-lg items-center justify-center">
+            <Text className="text-white">Generate New</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="bg-orange-200 w-80 h-80 border-4 rounded-lg z-10 absolute bottom-60 left-16"></View>
+      </View>
+    </>
   );
 }
+
+console.log(passwordStrength("5E£?930vt£V<").value);
